384dc423bdc271abfeaa8d4f82eb1da85e89bbaa
Timo Leinonen <splinecl@gmail.com>
2015-06-16 15:03:45 +0200

    Better greeting.

--- src/main/java/dk/kmd/spring/docker/GreetingController.java
+++ src/main/java/dk/kmd/spring/docker/GreetingController.java
@@ -1,5 +1,7 @@
 package dk.kmd.spring.docker;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, I am %s!";
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        try {
+            return String.format(template, InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "Whoopse";
     }
 }
